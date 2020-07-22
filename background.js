3e16895570b7e0f1c352e29d00b988ac4594a857
Chris Rowles <me@rowles.ch>
2020-07-22 07:29:59 +0100

    Loads of shit.

--- background.js
+++ background.js
@@ -1,21 +1,17 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'metro.co.uk'},
-          }),
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostSuffix: 'dailymail.co.uk' }
-          }),
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {hostEquals: 'metro.co.uk'},
+                }),
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {hostSuffix: 'dailymail.co.uk'}
+                }),
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
 });
--- popup.js
+++ popup.js
@@ -1,15 +1,9 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
-let removeIt = document.getElementById('removeIt');
-
-removeIt.onclick = function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: "let wr=document.querySelector('.wrapper-3AzfF');wr.parentNode.removeChild(wr);let body=document.getElementsByTagName('body')[0];body.style.overflow=\"scroll\";let ol=document.querySelector('.overlay-34_Kj');ol.parentNode.removeChild(ol);"});
-  });
-};
+document.addEventListener('DOMContentLoaded', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.tabs.executeScript(tabs[0].id, {
+            code: "let wr=document.querySelector('.wrapper-3AzfF');wr.parentNode.removeChild(wr);let body=document.getElementsByTagName('body')[0];body.style.overflow=\"scroll\";let ol=document.querySelector('.overlay-34_Kj');ol.parentNode.removeChild(ol);"
+        });
+    });
+});
